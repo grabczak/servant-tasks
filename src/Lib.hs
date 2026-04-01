e6014a43baa0c009ea1bab1c08f599b38705fef9
grabczak <kacpergrabczak@gmail.com>
2026-04-01 21:09:22 +0200

    Remove obsolete code

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Lib (createHash, verifyHash, createSession) where
+module Lib (createHash, verifyHash) where
 
 import Data.Password.Argon2
 import qualified Data.Text as T
-
-import qualified Data.ByteString.Lazy.Char8 as BSC
-import Data.Time.Clock
-import Servant
-import Servant.Auth.Server
-
-import Api
 
 createHash :: String -> IO String
 createHash str = do
@@ -25,16 +18,3 @@ verifyHash str hash = do
   return $ case checkPassword plain hashed of
     PasswordCheckSuccess -> True
     _ -> False
-
-createSession :: CookieSettings -> JWTSettings -> UserToken -> IO (Either ServerError (AuthHeaders String))
-createSession cookieSettings jwtSettings user = do
-  loginAccepted <- acceptLogin cookieSettings jwtSettings user
-  case loginAccepted of
-    Nothing -> return $ Left err401{errBody = "Login failed"}
-    Just headerBuilder -> do
-      now <- getCurrentTime
-      let expiry = addUTCTime (secondsToNominalDiffTime 2592000) now -- One month expiry
-      jwt <- makeJWT user jwtSettings (Just expiry)
-      case jwt of
-        Left _ -> return $ Left err401{errBody = "JWT creation failed"}
-        Right token -> return $ Right $ headerBuilder (BSC.unpack token)
